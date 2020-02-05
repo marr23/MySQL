7f57f368915c5b2fb5e8d42f906564def851791c
marr23 <59928671+marr23@users.noreply.github.com>
2020-02-05 20:00:05 +0700

    Add files via upload

---
+++ fave_films.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO fave_films VALUES (1, "500 days of Summer","Romantic",2009);
+INSERT INTO fave_films VALUES (2, "The Age of Adaline","Romantic-SciFiction",2015);
+INSERT INTO fave_films VALUES (3, "About Time","Romantic",2013);
+INSERT INTO fave_films VALUES (4, "Big Hero 6","Fiction",2014);
+INSERT INTO fave_films VALUES (5, "Up","Fantacy",2009),
+INSERT INTO fave_films VALUES (6, "The Martian","Science Fiction",2015)
+INSERT INTO fave_films VALUES (7, "
